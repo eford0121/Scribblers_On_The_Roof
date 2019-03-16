b761957ed9d19392b53043d2a47836f53245dfe3
eford0121 <41659185+eford0121@users.noreply.github.com>
2019-03-16 12:49:46 -0500

    Working flask (ugly)

--- app.py
+++ app.py
@@ -1,114 +1,95 @@
-#  import all the nececessary machine learning library and function
-#  scientific computing library for saving, reading, and resizing images
-from scipy.misc.pilutil import imsave, imread, imresize
-from flask import Flask, render_template,request
-#for matrix math
-import numpy as np
-#for importing our keras model
-import keras.models
-#for regular expressions, saves time dealing with string data
-import base64
-import re
-import cv2 as cv
-import sys
 import os
-from os import walk, getcwd
-# add system path relative to this flask app
-sys.path.append(os.path.abspath("./model"))
-from model import *
+from flask import Flask, request, jsonify
+
+import keras
+from keras.preprocessing import image
+from keras import backend as K
 
 app = Flask(__name__)
-global model
-# initializing the model
-model, graph = init()
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
-# create a path to the data folder: numpy image files
-Training_data_path = "data/"
-File_Name_list = []
-for (dirpath, dirnames, filenames) in walk(Training_data_path):
-    if filenames != '.DS_Store':
-        File_Name_list.extend(filenames)
-        break
-# implement python built in function : enumerate to loop through the image items, them use the index to loop through the remaining element.
-# we are not changing the list we are simply modifying the objects in the list(changind data file type from .npy to png)
-# converting image to string
-for ndx, member in enumerate(File_Name_list):
-    File_Name_list[ndx] = File_Name_list[ndx].replace('.npy', '')
-
-# create an image by using the  string
-def convertImage(ImageData):
-    Image_string = re.search(b'base64,(.*)',ImageData).group(1)
-    with open('output.png','wb') as output:
-      output.write(base64.b64decode(Image_string))
+model = None
+graph = None
 
 
-@app.route('/')
-def index():
-    return render_template("index.html")
+# Loading a keras model with flask
+# https://blog.keras.io/building-a-simple-keras-deep-learning-rest-api.html
+def load_model():
+    global model
+    global graph
+    model = keras.models.load_model("quickdraw.h5")
+    graph = K.get_session().graph
 
 
-@app.route('/predict/', methods=['GET', 'POST'])
-def predict():
+load_model()
 
-# get data from drawing canvas and save as image
-    Draw_Image = request.get_data()
-    convertImage(Draw_Image)
-    print("debug")
-#  # read parsed image back in 8-bit, black and white mode (L)
-    x = imread('output.png', mode='L')
 
-    x = preprocess(x)
+def prepare_image(img):
+    # Convert the image to a numpy array
+    img = image.img_to_array(img)
+    # Scale from 0 to 255
+    img /= 255
+    # Invert the pixels
+    img = 1 - img
+    # Flatten the image to an array of pixels
+    image_array = img.flatten().reshape(-1, 28 * 28)
+    image_array = img.flatten().reshape(1,1,28,28)
 
-    x = imresize(x, (32, 32))
+    # Return the processed feature array
+    return image_array
 
-    x = x.astype('float32')
-    x /= 255
 
-    x = x.reshape(1, 32, 32, 1)
-    print("debug2")
-
-    with graph.as_default():
-
-        out = model.predict(x)
-        print(out)
-# Returns the indices of the maximum values along an axis.
-        print(np.argmax(out, axis=1))
-        index = np.array(np.argmax(out, axis=1))
-        index = index[0]
-        sketch = File_Name_list[index]
-        print("debug3")
-        return sketch
-
-@app.route('/getNoteText',methods=['GET','POST'])
-def GetNoteText():
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    data = {"success": False}
     if request.method == 'POST':
-        file = request.files['pic']
-        filename = file.filename
-        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        processImage(filename)
-        return "Success"
-    else:
-        return "No Success"
+        print(request)
 
-# Gamma correction and the Power Law Transform
-# image pixel intensities must be scaled from the range [0, 255] to [0, 1.0].
-def adjust_gamma(image, gamma=1.5):
-# build a lookup table mapping the pixel values [0, 255] to  their adjusted gamma values
-   invGamma = 1.0 / gamma
-   table = np.array([((i / 255.0) ** invGamma) * 255
-      for i in np.arange(0, 256)]).astype("uint8")
-# apply gamma correction using the lookup table
-   return cv.LUT(image, table)
+        if request.files.get('file'):
+            # read the file
+            file = request.files['file']
 
+            # read the filename
+            filename = file.filename
 
-def preprocess(img):
-  
-# Adaptive Thresholding
-# threshold value is the weighted sum of neighbourhood values where weights are a gaussian window.
-    th3 = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C,
-                               cv.THRESH_BINARY_INV, 11, 2)
-    return th3
+            # create a path to the uploads folder
+            filepath = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+
+            # Save the file to the uploads folder
+            file.save(filepath)
+
+            # Load the saved image using Keras and resize it to the mnist
+            # format of 28x28 pixels
+            image_size = (28, 28)
+            im = image.load_img(filepath, target_size=image_size,
+                                grayscale=True)
+
+            # Convert the 2D image to an array of pixel values
+            image_array = prepare_image(im)
+            print(image_array)
+
+            # Get the tensorflow default graph and use it to make predictions
+            global graph
+            with graph.as_default():
+
+                # Use the model to make a prediction
+                predicted_digit = model.predict_classes(image_array)[0]
+                data["prediction"] = str(predicted_digit)
+
+                # indicate that the request was a success
+                data["success"] = True
+
+            return jsonify(data)
+    return '''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+
 
 if __name__ == "__main__":
-	
-	app.run(debug=True)  
+    app.run(debug=True)
